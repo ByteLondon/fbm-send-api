097b6c47117d7d2c447927f3cfac0a8766ca07ed
Ian Phillips <ianp@ianp.org>
2016-10-18 12:13:43 +0100

    Initial commit
    Code imported from existing project(s).

---
+++ index.js
@@ -0,0 +1,93 @@
+const {filter, isEmpty, isString, map, pickBy, truncate} = require('lodash')
+
+const ensureLength = (name, maxLen, s) => {
+  if (isString(s) && s.length > maxLen) {
+    const message = `${name} must be ≤ ${maxLen} characters: "${truncate(s)}" (${s.length})`
+    throw new Error(message)
+  }
+  return s
+}
+
+const prepareImageURI = uri => {
+  if (!isString(uri)) {
+    return undefined
+  } else {
+    return encodeURI(uri)
+  }
+}
+
+const mkButton = (title, payload) => ({
+  type: 'postback',
+  title: ensureLength('title', 20, title),
+  payload: ensureLength('payload', 1000, payload)
+})
+
+const mkLinkButton = (title, url, options) => Object.assign({
+  type: 'web_url',
+  title: ensureLength('title', 20, title),
+  url: encodeURI(url)
+}, options)
+
+const mkPhoneNumberButton = (title, number, options) => Object.assign({
+  type: 'phone_number',
+  title: ensureLength('title', 20, title),
+  payload: number.startsWith('+44') ? number : `+44${number}`
+}, options)
+
+const mkTextMessage = (text, options) => Object.assign({
+  text: ensureLength('text', 320, text)
+}, options)
+
+const mkImageMessage = (url, options) => Object.assign({
+  attachment: {
+    type: 'image',
+    payload: {url: prepareImageURI(url)}
+  }
+}, options)
+
+const mkQuickReplies = replies => ({
+  quick_replies: map(replies, (payload, title) => ({content_type: 'text', title, payload}))
+})
+
+const mkElement = (title, subtitle, imageUrl, ...buttons) => {
+  const b = filter(buttons)
+  return pickBy({
+    title: ensureLength('title', 80, title),
+    subtitle: ensureLength('subtitle', 80, subtitle),
+    image_url: prepareImageURI(imageUrl),
+    buttons: isEmpty(b) ? null : b
+  })
+}
+
+const mkButtonTemplate = (text, buttons, options) => {
+  const b = filter(buttons)
+  return Object.assign({
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: ensureLength('text', 320, text),
+        buttons: isEmpty(b) ? null : b
+      }
+    }
+  }, options)
+}
+
+const mkGenericTemplate = (elements, options) => Object.assign({
+  attachment: {
+    type: 'template',
+    payload: { template_type: 'generic', elements }
+  }
+}, options)
+
+module.exports = {
+  mkButton,
+  mkButtonTemplate,
+  mkElement,
+  mkGenericTemplate,
+  mkImageMessage,
+  mkLinkButton,
+  mkPhoneNumberButton,
+  mkQuickReplies,
+  mkTextMessage
+}
