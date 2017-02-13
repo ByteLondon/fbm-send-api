a5eac3d1af950cad04efcb55c5b7e7f7cf04308d
Ian Phillips <ianp@ianp.org>
2017-02-13 15:28:22 +0000

    Relax type constraints

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
 export function mkButton(title: string, payload: string)
 
-export function mkLinkButton(title: string, url: string, options: Object)
+export function mkLinkButton(title: string, url: string, options?: Object)
 
-export function mkPhoneNumberButton(title: string, number: string, options: Object)
+export function mkPhoneNumberButton(title: string, number: string, options?: Object)
 
-export function mkTextMessage(text: string, options: Object)
+export function mkTextMessage(text: string, options?: Object)
 
-export function mkImageMessage(url: string, options: Object)
+export function mkImageMessage(url: string, options?: Object)
 
-export function mkVideoMessage(url: string, options: Object)
+export function mkVideoMessage(url: string, options?: Object)
 
 export function mkQuickRepliesreplies(qrs: Object)
 
 export function mkElement(title: string, subtitle: string, imageUrl?: string, ...buttons)
 
-export function mkButtonTemplate(text: string, buttons: Object, options: Object)
+export function mkButtonTemplate(text: string, buttons: Object, options?: Object)
 
-export function mkGenericTemplate(elements: Array<Object>, options: Object)
+export function mkGenericTemplate(elements: Array<Object>, options?: Object)
